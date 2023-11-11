c577e0f8ed3821ef2e7a7a12c776e698e717273c
Abdallah Mohamed <140225887+AbdallahM19@users.noreply.github.com>
2023-11-11 17:14:51 +0200

    Update file_io_functions.c

--- file_io_functions.c
+++ file_io_functions.c
@@ -5,21 +5,24 @@
  * @info: parameter struct
  * Return: allocated string containg history file
 */
-char *get_history_file (info_t *info)
+char *get_history_file(info_t *info)
 {
-	char *buf, *dir;
+    char *buf, *dir;
 
-	dir = _getenv(info, "HOME=");
-	if (!dir)
-		return (NULL);
-	buf = malloc(sizeof(char) * (_strlength(dir) + _strlength(HIST_FILE) + 2));
-	if (buf)
-		return (NULL);
-	buf[0] = 0;
-	_strcpy(buf, dir);
-	_strcat(buf, "/");
-	strcat(buf, HIST_FILE);
-	return (buf);
+    dir = _getenv(info, "HOME=");
+    if (!dir)
+        return NULL;
+
+    buf = malloc(sizeof(char) * (_strlength(dir) + _strlength(HIST_FILE) + 2));
+    if (!buf)
+        return NULL;
+
+    buf[0] = '\0';
+    _strcpy(buf, dir);
+    _strcat(buf, "/");
+    strcat(buf, HIST_FILE);
+
+    return buf;
 }
 
 /**
@@ -29,24 +32,28 @@ char *get_history_file (info_t *info)
  */
 int write_history(info_t *info)
 {
-	ssize_t fd;
-	char *filename = get_history_file(info);
-	list_t *node = NULL;
+    ssize_t fd;
+    char *filename = get_history_file(info);
+    list_t *node = NULL;
 
-	if (filename)
-		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-	free(filename);
-	if (fd == -1)
-		return (-1);
-	for (node = info->history; node; node = node->next)
-	{
-		_putsfd(node->str, fd);
-		_putfd('\n', fd);
-	}
-	_putfd(BUF_FLUSH, fd);
-	close(fd);
-	return (1);
+    if (!filename)
+        return -1;
+
+    fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+    free(filename);
+
+    if (fd == -1)
+        return -1;
+
+    for (node = info->history; node; node = node->next)
+    {
+        _putsfd(node->str, fd);
+        _putfd('\n', fd);
+    }
+    _putfd(BUF_FLUSH, fd);
+    close(fd);
+
+    return 1;
 }
 
 /**
@@ -54,46 +61,48 @@ int write_history(info_t *info)
  * @info: the parameter struct
  * Return: histcount on success, 0 otherwise
 */
-int read_history (info_t *info)
+int read_history(info_t *info)
 {
-	int i, last = 0, linecount = 0;
-	ssize_t fd, rdlen, fsize= 0;
-	struct stat st;
-	char *buf = NULL, *filename = get_history_file(info);
+    int i, last = 0, linecount = 0;
+    ssize_t fd, rdlen, fsize = 0;
+    struct stat st;
+    char *buf = NULL, *filename = get_history_file(info);
 
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	free(filename);
-	if (fd == -1)
-		return (0);
-	if (!fstat (fd, &st))
-		fsize = st.st_size;
-	if (fsize < 2)
-		return (0);
-	buf = malloc(sizeof(char) * (fsize + 1));
-	if (!buf)
-		return (0);
-	rdlen = read(fd, buf, fsize);
-	buf[fsize] = 0;
-	if (rdlen <= 0)
-		return (free(buf), 0);
-	close (fd);
-	for (i = 0; i < fsize; i++)
-		if (buf[i] == '\n')
-		{
-			buf[i] = 0;
-			build_history_list(info, buf + last, linecount++);
-			last = i + 1;
-		}
-	if (last != i)
-		build_history_list(info, buf + last, linecount++);
-	free(buf);
-	info->histcount = linecount;
-	while (info->histcount-- >= HIST_MAX)
-		delete_node_at_index(&(info->history), 0);
-	renumber_history(info);
-	return (info->histcount);
+    if (!filename)
+        return 0;
+    fd = open(filename, O_RDONLY);
+    free(filename);
+    if (fd == -1)
+        return 0;
+    if (!fstat(fd, &st))
+        fsize = st.st_size;
+    if (fsize < 2)
+        return 0;
+    buf = malloc(sizeof(char) * (fsize + 1));
+    if (!buf)
+        return 0;
+    rdlen = read(fd, buf, fsize);
+    buf[fsize] = '\0';
+    if (rdlen <= 0)
+        return free(buf), 0;
+    close(fd);
+    for (i = 0; i < fsize; i++)
+    {
+        if (buf[i] == '\n')
+        {
+            buf[i] = '\0';
+            build_history_list(info, buf + last, linecount++);
+            last = i + 1;
+        }
+    }
+    if (last != i)
+        build_history_list(info, buf + last, linecount++);
+    free(buf);
+    info->histcount = linecount;
+    while (info->histcount-- >= HIST_MAX)
+        delete_node_at_index(&(info->history), 0);
+    renumber_history(info);
+    return info->histcount;
 }
 
 /**
