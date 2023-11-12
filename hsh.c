bee50ceec5a6ab3c4d9b5bb6ca7a29b47d9fc2b6
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-12 05:21:13 -0800

    shell

--- hsh.c
+++ hsh.c
@@ -11,7 +11,6 @@ int hsh(info_t *info, char **av)
 	ssize_t r = 0;
 	int builtin_ret = 0;
 
-<<<<<<< HEAD
 	while (r != -1 && builtin_ret != -2)
 	{
 		clear_info(info);
@@ -41,38 +40,6 @@ int hsh(info_t *info, char **av)
 		exit(info->err_num);
 	}
 	return (builtin_ret);
-=======
-    while (r != -1 && builtin_ret != -2)
-    {
-        clear_info(info);
-        if (interactive(info))
-            _puts("$ ");
-        _eputchar(BUF_FLUSH);
-        r = get_input(info);
-        if (r != -1)
-        {
-            set_info(info, av);
-            builtin_ret = find_builtin(info);
-            if (builtin_ret == -1)
-                find_cmd(info);
-        }
-        else if (interactive(info))
-            _putchar('\n');
-        free_info(info, 0);
-    }
-    free_info(info, 1);
-    write_history(info);
-
-    if (!interactive(info) && info->status)
-        exit(info->status);
-    if (builtin_ret == -2)
-    {
-        if (info->err_num == -1)
-            exit(info->status);
-        exit(info->err_num);
-    }
-    return (builtin_ret);
->>>>>>> 3093c1f2a78cdc34a9cbd3b4137c03c49265a5b9
 }
 
 /**
