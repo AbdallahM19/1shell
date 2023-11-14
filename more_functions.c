95b9a969f35ae840da39de0fde10a67f8e46a984
Abdallah Mohamed <140225887+AbdallahM19@users.noreply.github.com>
2023-11-14 13:29:15 +0200

    Update more_functions.c

--- more_functions.c
+++ more_functions.c
@@ -1,11 +1,23 @@
 #include"shell.h"
+
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
 /**
  * is_delim checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
 */
-int is_delim (char c, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
@@ -18,7 +30,7 @@ int is_delim (char c, char *delim)
  * @c: The character to input
  * Return: 1 if c is alphabetic, 0 otherwise
 */
-int isalpha (int c)
+int isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
