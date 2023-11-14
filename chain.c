3ec45cbe930c82901b0b6b6df5f96b7450272166
Abdallah Mohamed <140225887+AbdallahM19@users.noreply.github.com>
2023-11-14 04:49:32 +0200

    Update chain.c

--- chain.c
+++ chain.c
@@ -9,29 +9,29 @@
 */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
-	size_t j = *p;
+    size_t i = *p;
 
-	if (buf[j] == '|' && buf[j + 1] == '|')
-	{
-		buf[j] = 0;
-		j++;
-		info->cmd_buf_type = CMD_OR;
-	}
-	else if (buf[j] == '&' && buf[j+1] == '&')
-	{
-		buf[j] = 0;
-		j++;
-		info->cmd_buf_type = CMD_AND;
-	}
-	else if (buf[j] == ';')
-	{
-		buf[j] = 0;
-		info->cmd_buf_type = CMD_CHAIN;
-	}
-	else
-	return (0);
-	*p = j;
-	return (1);
+    if (buf[i] == '|' && buf[i + 1] == '|')
+    {
+        buf[i] = 0;
+        i++;
+        info->cmd_buf_type = CMD_OR;
+    }
+    else if (buf[i] == '&' && buf[i + 1] == '&')
+    {
+        buf[i] = 0;
+        i++;
+        info->cmd_buf_type = CMD_AND;
+    }
+    else if (buf[i] == ';')
+    {
+        buf[i] = 0;
+        info->cmd_buf_type = CMD_CHAIN;
+    }
+    else
+        return 0;
+    *p = i;
+    return 1;
 }
 
 /**
@@ -46,20 +46,20 @@ int is_chain(info_t *info, char *buf, size_t *p)
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
-	(void) i;
+
 	if (info->cmd_buf_type == CMD_AND)
 	{
 		if (info->status)
 		{
-			buf[j] = 0;
+			buf[i] = 0;
 			j = len;
 		}
 	}
 	if (info->cmd_buf_type == CMD_OR)
 	{
-		if (info->status)
+		if (!info->status)
 		{
-			buf[j] = 0;
+			buf[i] = 0;
 			j = len;
 		}
 	}
@@ -111,23 +111,23 @@ int replace_vars(info_t *info)
 		if (!_strcmp(info->argv[i], "$?"))
 		{
 			replace_string(&(info->argv[i]),
-			_strdup(convert_number(info->status, 10, 0)));
+				_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
 		if (!_strcmp(info->argv[i], "$$"))
 		{
 			replace_string(&(info->argv[i]),
-			_strdup(convert_number(getpid(), 10, 0)));
+				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
 		node = node_starts_with(info->env, &info->argv[i][1], '=');
 		if (node)
 		 {
 			replace_string(&(info->argv[i]),
-			_strdup(_strchr(node->str, '=') + 1));
+				_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
-		replace_string(&(info->argv[i]),_strdup(""));
+		replace_string(&info->argv[i], _strdup(""));
 	}
 	return (0);
 }
@@ -141,8 +141,6 @@ int replace_vars(info_t *info)
 int replace_string(char **old, char *new)
 {
 	free(*old);
-
 	*old = new;
-
 	return (1);
 }
