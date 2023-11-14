1282218be164f729dade5db628377708d677af49
Abdallah Mohamed <140225887+AbdallahM19@users.noreply.github.com>
2023-11-14 06:04:41 +0200

    Update getline.c

--- getline.c
+++ getline.c
@@ -32,7 +32,6 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 			info->linecount_flag = 1;
 			_commentremover(*buf);
 			build_history_list(info, *buf, info->histcount++);
-			/* if (_strchr(*buf, ';')) is a cmmand chain?*/
 			{
 				*len = r;
 				info->cmd_buf = buf;
@@ -54,6 +53,7 @@ ssize_t get_input(info_t *info)
 	static size_t i, j, len;
 	ssize_t r = 0;
 	char **buf_p = &(info->arg), *p;
+
 	_putchar(BUF_FLUSH);
 	r = input_buf(info, &buf, &len);
 	if (r == -1)
@@ -96,7 +96,7 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 	if (*i)
 		return (0);
 	r = read(info->readfd, buf, READ_BUF_SIZE);
-	if (r > 0)
+	if (r >= 0)
 		*i = r;
 	return (r);
 }
@@ -114,17 +114,17 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	size_t k;
 	ssize_t r = 0, s = 0;
 	char *p = NULL, *new_p = NULL, *c;
-	p = *ptr;
 
+	p = *ptr;
 	if (p && length)
 		s = *length;
 	if (i == len)
 		i = len = 0;
 	r = read_buf(info, buf, &len);
-	if (r == -1 || (r == 1 && len == 0))
+	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
 	c = _strchr(buf + i, '\n');
-	k = c ? 1 + (unsigned int) (c - buf) : len;
+	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
 	if (!new_p)
 		return (p ? free(p), -1 : -1);
@@ -132,7 +132,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 		_strncat(new_p, buf + i, k - i);
 	else
 		_strncpy(new_p, buf + i, k - i +1);
-	s += k = i;
+	s += k - i;
 	i = k;
 	p = new_p;
 	if (length)
