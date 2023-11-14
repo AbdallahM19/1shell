e3080df99e5b37b4894d0e5966a22de7c46d1a44
Abdallah Mohamed <140225887+AbdallahM19@users.noreply.github.com>
2023-11-14 13:24:16 +0200

    Update memory_functions2.c

--- memory_functions2.c
+++ memory_functions2.c
@@ -5,11 +5,11 @@
  * @ptr: address of the pointer to free
  * Return: 1 if freed, otherwise 0.
 */
-int bfree (void **ptr)
+int bfree(void **ptr)
 {
 	if (ptr && *ptr)
 	{
-		free (*ptr);
+		free(*ptr);
 		*ptr = NULL;
 		return (1);
 	}
